4c57d30320bcdacd30b2003ae8c1ee841011fcbf
Mayur Ryali <mayurryali29@gmail.com>
2022-06-03 18:43:11 -0700

    Update to read Large File

--- indexer/src/main/java/org/example/Indexer.java
+++ indexer/src/main/java/org/example/Indexer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
@@ -17,28 +18,43 @@ import java.util.Map;
 public class Indexer {
     IndexWriter indexWriter = null;
 
-    void parseJSON() {
-        Gson gson = new Gson();
+//    void parseJSON() {
+//        Gson gson = new Gson();
+//
+//        Reader reader;
+//        try {
+//            reader = Files.newBufferedReader(Paths.get("C:/Users/mayur/Desktop/Coding/CS172/WebSearch/indexer/tweets-fixed.json"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        Map<?, ?> jsonMap = gson.fromJson(reader, Map.class);
+//        for (Map.Entry<?, ?> entry : jsonMap.entrySet()) {
+//            System.out.println(entry.getKey() + "=" + entry.getValue());
+//        }
+//
+//        // close reader
+//        try {
+//            reader.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-        Reader reader;
-        try {
-            reader = Files.newBufferedReader(Paths.get("tweets.json"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        Map<?, ?> jsonMap = gson.fromJson(reader, Map.class);
-        for (Map.Entry<?, ?> entry : jsonMap.entrySet()) {
-            System.out.println(entry.getKey() + "=" + entry.getValue());
-        }
-
-        // close reader
-        try {
-            reader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void parseJSON() throws IOException {
+        try (
+                InputStream inputStream = Files.newInputStream(Path.of("C:/Users/mayur/Desktop/Coding/CS172/WebSearch/indexer/small_tweets.json"));
+                JsonReader reader = new JsonReader(new InputStreamReader(inputStream));
+        ) {
+            reader.beginArray();
+            while (reader.hasNext()) {
+                Map<?,?> map = new Gson().fromJson(reader, Map.class);
+                System.out.println(map);
+            }
+            reader.endArray();
         }
     }
+
     void buildIndex() {
         Directory indexDirectory;
         try {
@@ -57,7 +73,11 @@ public class Indexer {
             throw new RuntimeException(e);
         }
 
-        parseJSON();
+        try {
+            parseJSON();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             indexWriter.commit();
